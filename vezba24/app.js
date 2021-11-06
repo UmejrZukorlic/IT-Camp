96854d9c92025bec86b30b8c98f964f35efefc4d
Umejr Zukorlić <zukorlicumejr@gmail.com>
2021-11-06 13:25:04 +0100

    first commit

---
+++ vezba24/app.js
@@ -0,0 +1,7 @@
+var1 = [];
+pocetniUnos = parseInt(prompt("Unesi pocetni broj"))
+zavrsniUnos = parseInt(prompt("Unesi zavrsni unos"))
+for (i = pocetniUnos; i <= zavrsniUnos; i++) {
+    var1.push(i);
+    console.log(var1);
+}
