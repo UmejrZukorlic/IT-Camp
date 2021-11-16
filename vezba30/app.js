29d9307ed044c971cab61ac6d7d0c4db17e4a2f0
Umejr Zukorlić <zukorlicumejr@gmail.com>
2021-11-16 20:50:13 +0100

    first commit

---
+++ vezba30/app.js
@@ -0,0 +1,19 @@
+// var1 = {
+//     ime: "Umjer",
+//     prezime: "Zukorlic",
+//     godine: "17"
+// }
+// console.log(var1);
+// ime = prompt("Unesi ime")
+// ime2 = ime.trim(),
+//     prezime = prompt("unesi prezime")
+// prezime2 = prezime.trim()
+// var1 = {
+//     firstname: ime2,
+//     lastname: prezime2
+// }
+// console.log(var1);
+// delete var1.firstname
+// console.log(var1);
+unos = parseInt(prompt("Unesi broj"))
+console.log(unos * -1);
