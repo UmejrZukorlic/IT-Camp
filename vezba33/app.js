2ab5df51c15c3f4b793cb956311ca57adbc53713
Umejr Zukorlić <zukorlicumejr@gmail.com>
2021-11-18 21:15:14 +0100

    first commit

---
+++ vezba33/app.js
@@ -0,0 +1,11 @@
+// function mojaFunkcija() {
+//     console.log("Funkcija se izvrsila");
+// }
+// mojaFunkcija();
+br1 = parseInt(prompt("unesi prvi broj"))
+br2 = parseInt(prompt("unesi drugi broj"))
+
+function mojaFunkcija(a, b) {
+    console.log("Zbir je", a + b);
+}
+mojaFunkcija(br1, br2);
