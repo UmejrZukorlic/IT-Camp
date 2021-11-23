7ba62ee8d85ab23258ca5ccc8b7258694ab721a1
Umejr Zukorlić <zukorlicumejr@gmail.com>
2021-11-23 21:15:14 +0100

    first commit

---
+++ vezba37/app.js
@@ -0,0 +1,35 @@
+// function getDivisorsCnt(n) {
+//     brojac = 0
+//     for (i = 1; i <= n; i++) {
+//         if (n % i === 0) {
+//             brojac++
+//         }
+//     }
+//     return brojac;
+// }
+// ---------------------------------------------------------------------------------------------
+
+// function cookingTime(eggs) {
+//     return Math.ceil(eggs / 8) * 5
+// }
+
+// ---------------------------------------------------------------------------------------------
+// const sequenceSum = (begin, end, step) => {
+//     zbir = 0
+//     for (i = begin; i <= end; i++) {
+//         zbir += i
+//         i += step - 1
+//     }
+//     return zbir
+// };
+
+// ---------------------------------------------------------------------------------------------
+
+// function between(a, b) {
+//     arr=[]
+//     for(i=a;i<=b;i++){
+//       arr.push(i)
+//     }
+//     // your code here
+//     return arr
+// }
