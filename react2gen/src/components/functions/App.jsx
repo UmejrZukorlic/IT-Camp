25e4b1842543cfcdb0af49594c57dbede3da1d59
Umejr Zukorlić <zukorlicumejr@gmail.com>
2022-06-30 18:54:13 +0200

    first class

---
+++ react2gen/src/components/functions/App.jsx
@@ -0,0 +1,8 @@
+function AppF(props) {
+    return <div style={{width:"300px", height:"450px", border:"1px black solid"}}>
+        <div style={{width:"100%",height:"70%",backgroundColor:"red"}}></div>
+        <h1>Umejr Zukorlic</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+    </div>
+}
+export default AppF;
